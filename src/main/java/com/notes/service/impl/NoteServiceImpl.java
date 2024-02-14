fcae051ddd8246e2b68920d0fb0336ca06f6e12e
Artemie Fliostor <artiom.fliostor@gmail.com>
2024-02-14 15:52:51 +0200

    Refactor

--- src/main/java/com/notes/service/impl/NoteServiceImpl.java
+++ src/main/java/com/notes/service/impl/NoteServiceImpl.java
@@ -11,26 +11,34 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class NoteServiceImpl implements NoteService {
-    private final NoteRepository noteRepository;
+  private final NoteRepository noteRepository;
 
-    @Override
-    public Note findBy(Long id, Long userId) {
-        return noteRepository.findByIdAndUserId(id, userId).orElseThrow(() -> new ResourceNotFoundException("Note with id[%s] and userId[%s] not found.".formatted(id, userId)));
-    }
+  @Override
+  public Note findBy(Long id, Long userId) {
+    return noteRepository
+        .findByIdAndUserId(id, userId)
+        .orElseThrow(
+            () ->
+                new ResourceNotFoundException(
+                    "Note with id[%s] and userId[%s] not found.".formatted(id, userId)));
+  }
 
-    @Override
-    public Note findBy(Long id) {
-        return noteRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Note with id[%s] not found.".formatted(id)));
-    }
+  @Override
+  public Note findBy(Long id) {
+    return noteRepository
+        .findById(id)
+        .orElseThrow(
+            () -> new ResourceNotFoundException("Note with id[%s] not found.".formatted(id)));
+  }
 
-    @Override
-    public Note create(Note note) {
-        note.setCreationDate(OffsetDateTime.now());
-        return noteRepository.save(note);
-    }
+  @Override
+  public Note create(Note note) {
+    note.setCreationDate(OffsetDateTime.now());
+    return noteRepository.save(note);
+  }
 
-    @Override
-    public Note update(Note note) {
-        return null;
-    }
+  @Override
+  public Note update(Note note) {
+    return null;
+  }
 }
